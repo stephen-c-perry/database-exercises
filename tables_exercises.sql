59c59dcfd28a4161706e9289671a834df0d2a35a
stephen-c-perry <stephen.christian.perry@gmail.com>
2022-10-24 13:49:22 -0500

    added solution

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -1,16 +1,4 @@
 show databases;
-USE zillow;
-select database();
-show databases;
-use fruits_db;
-select database();
-show databases;
-use tsa_item_demand;
-select database();
-select item_brand from items;
-select store_address from stores;
-select sale_id from sales where sale_amount > 15;
-select item_name from items where item_price < 5;
 
 -- single line comment
 
@@ -40,3 +28,16 @@ Many to Many
 */
 
 SHOW CREATE TABLE dept_manager;
+
+/*
+CREATE TABLE `dept_manager` (
+  `emp_no` int NOT NULL,
+  `dept_no` char(4) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL,
+  PRIMARY KEY (`emp_no`,`dept_no`),
+  KEY `dept_no` (`dept_no`),
+  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+*/
