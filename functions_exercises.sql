c88be109258fbff37c00c2080ca55568eb7525c5
stephen-c-perry <stephen.christian.perry@gmail.com>
2022-10-25 16:50:23 -0500

    started functions exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,4 +1,6 @@
-/* Copy the order by exercise and save it as functions_exercises.sql.
+use employees;
+
+Copy the order by exercise and save it as functions_exercises.sql.
 
 Write a query to to find all employees whose last name starts and ends with 'E'. Use concat() to combine their first and last name together as a single column named full_name.
 
@@ -9,7 +11,20 @@ Find all employees hired in the 90s and born on Christmas. Use datediff() functi
 Find the smallest and largest current salary from the salaries table.
 
 Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like:
-*/
 
-use employees;
 
++------------+------------+-----------+------------+
+| username   | first_name | last_name | birth_date |
++------------+------------+-----------+------------+
+| gface_0953 | Georgi     | Facello   | 1953-09-02 |
+| bsimm_0664 | Bezalel    | Simmel    | 1964-06-02 |
+| pbamf_1259 | Parto      | Bamford   | 1959-12-03 |
+| ckobl_0554 | Chirstian  | Koblick   | 1954-05-01 |
+| kmali_0155 | Kyoichi    | Maliniak  | 1955-01-21 |
+| apreu_0453 | Anneke     | Preusig   | 1953-04-20 |
+| tziel_0557 | Tzvetan    | Zielinski | 1957-05-23 |
+| skall_0258 | Saniya     | Kalloufi  | 1958-02-19 |
+| speac_0452 | Sumant     | Peac      | 1952-04-19 |
+| dpive_0663 | Duangkaew  | Piveteau  | 1963-06-01 |
++------------+------------+-----------+------------+
+10 rows in set (0.05 sec)
