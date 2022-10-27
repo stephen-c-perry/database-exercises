0401ac7bc06454bc97f518de619db1940bf9f890
stephen-c-perry <stephen.christian.perry@gmail.com>
2022-10-27 15:18:10 -0500

    added exercise questions

---
+++ subqueries_exercises.sql
@@ -0,0 +1,29 @@
+/*
+
+1. Find all the current employees with the same hire date as employee 101010 using a sub-query.
+
+/*
+2. Find all the titles ever held by all current employees with the first name Aamod.*/
+
+/*
+3. How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.*/
+
+/*
+4. Find all the current department managers that are female. List their names in a comment in your code.*/
+
+/*
+5. Find all the employees who currently have a higher salary than the companies overall, historical average salary.*/
+
+/*
+6. How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this?
+
+Hint You will likely use multiple subqueries in a variety of ways
+Hint It's a good practice to write out all of the small queries that you can. Add a comment above the query showing the number of rows returned. You will use this number (or the query that produced it) in other, larger queries.*/
+
+/*
+BONUS
+
+Find all the department names that currently have female managers.
+Find the first and last name of the employee with the highest salary.
+Find the department name that the employee with the highest salary works in.
+*/
